aea6c89b3921df74ea6611795e09b0bee07bd3bb
admin <admin@DESKTOP-NF5BD85>
2020-08-03 11:31:51 +0900

    헬로우

---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("헬로우 자바!"); 
+       System.out.println("수정했음!");
+	}
+
+}
---
+++ First/src/hello/HelloName.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class HelloName {
+
+	public static void main(String[] args) {
+		System.out.println("Çï·Î¿ì ¿ùµå!");
+	}
+
+}
