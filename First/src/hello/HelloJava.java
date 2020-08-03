e8e59c2e17be1586b56eb84dd29ad6953305a37c
admin <admin@DESKTOP-NF5BD85>
2020-08-03 11:44:22 +0900

    헬로우 자바 수정 했음

--- First/src/hello/HelloJava.java
+++ First/src/hello/HelloJava.java
@@ -6,6 +6,7 @@ public class HelloJava {
 		// TODO Auto-generated method stub
        System.out.println("헬로우 자바!"); 
        System.out.println("수정했음!");
+       System.out.println("깃허브 올리고 수정함!");
 	}
 
 }
